e71690dcfdf8f59cdd8e8472de48a9cfd2d43440
zhymgc <1026467712@qq.com>
2020-03-11 22:32:13 +0800

    Initial commit

---
+++ microservicecloud-api/src/main/java/com/zhymgc/entities/Dept.java
@@ -0,0 +1,28 @@
+package com.zhymgc.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2020-3-11.
+ */
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Dept implements Serializable{
+
+    private Long deptno;
+
+    private String dname;
+
+    /**
+     * 来自哪个数据库
+     * 因为微服务可以一个服务对应一个数据库
+     * 同一信息存到不同库里
+     */
+    private String db_source;
+
+}
